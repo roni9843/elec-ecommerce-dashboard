b0b6ec56ec70ab02c2bfe1389bef3821a62ef7cf
roni9843 <ronironi98439843@gmail.com>
2024-08-25 00:43:30 +0600

    add text

--- src/index.js
+++ src/index.js
@@ -7,6 +7,28 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quae adipisci
+    iure pariatur maiores unde eveniet explicabo. Est doloremque voluptas
+    nostrum facilis voluptatibus expedita minima non delectus quisquam velit
+    quibusdam reiciendis optio dolorem esse harum et repellendus, temporibus
+    voluptates animi ut. Velit, facilis quae doloremque, veniam aliquam
+    asperiores voluptas molestias saepe mollitia alias earum pariatur nulla
+    explicabo, ducimus illo ex amet quam deleniti necessitatibus aliquid quas
+    quis omnis eligendi enim. Doloribus in nobis id veritatis saepe! Asperiores
+    at veniam distinctio incidunt aperiam commodi aut ipsam accusamus molestiae
+    tenetur blanditiis cumque, iure, fugit impedit accusantium, quo nam?
+    Distinctio inventore ratione nobis sunt dolorum pariatur esse natus,
+    necessitatibus numquam cumque sint quo sapiente explicabo magni illum
+    consectetur repellat nam? Nisi velit officiis numquam perferendis commodi
+    aspernatur natus dolores corporis dolorem culpa? Architecto harum ad eaque
+    aspernatur mollitia! Sint doloremque rerum debitis repellendus porro
+    perspiciatis eos ipsam, veniam numquam, voluptates at fugit totam dicta quia
+    enim ipsa repellat itaque quo aliquid, quidem ad accusamus! Doloremque
+    cupiditate minima, recusandae impedit maxime, a, voluptatibus optio quo
+    voluptatum suscipit distinctio non hic eligendi. Mollitia, rerum facilis
+    impedit voluptates quos vero? Quisquam, exercitationem iste dicta placeat
+    architecto blanditiis laboriosam. Pariatur id laudantium eligendi quasi
+    saepe, maxime consequatur.
     <App />
   </React.StrictMode>
 );
